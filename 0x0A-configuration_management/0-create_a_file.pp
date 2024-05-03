22ea26824ee0add416f790e310e4f5ccd8068e19
Matsadura <zidanditoo1@gmail.com>
2024-05-03 17:32:16 +0100

    [UPDATE] Remove tabs

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,6 @@
 file { '/tmp/school':
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0744',
-  content => 'I love Puppet',
+owner   => 'www-data',
+group   => 'www-data',
+mode    => '0744',
+content => 'I love Puppet',
 }
