3cff880ae31ba6a3798c30f082dcb80782ee6982
Matsadura <zidanditoo1@gmail.com>
2024-05-04 18:41:05 +0100

    [UPDATE] Add comment

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,5 @@
+# Configures ssh client to connect to a server without passowrd
+
 file { "/home/${::USER}/.ssh/config":
   ensure  => present,
   content => "Host web1\n User ubuntu\n Hostname 54.196.38.185\n PubkeyAuthentication yes\n IdentityFile ~/.ssh/school"
