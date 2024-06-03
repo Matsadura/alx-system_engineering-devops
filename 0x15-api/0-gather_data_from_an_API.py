7a23150d65e9c59cd9fb67c6e18af32bcd908ba8
Matsadura <zidanditoo1@gmail.com>
2024-06-03 12:47:57 +0100

    [ADD] Task 0: Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+if __name__ == "__main__":
+    user_id = int(sys.argv[1])
+    response_users = requests.get('https://jsonplaceholder.typicode.com/users')
+    employees = response_users.json()
+    employees_list = [user for user in employees if user['id'] == user_id]
+    user_name = employees_list[0]['name']
+
+    response_todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = response_todos.json()
+    user_x = [user for user in todos if user['userId'] == user_id]
+    completed = [user for user in user_x if user['completed'] is True]
+    print(f"Employee {user_name} is done with tasks\
+({len(completed)}/{len(user_x)}):")
+    for true in completed:
+        print(f"\t {true['title']}")
