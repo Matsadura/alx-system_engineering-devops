8212be2c63a78cafd30a2b9718d3b2dbe9ed12e6
Matsadura <zidanditoo1@gmail.com>
2024-06-18 19:26:12 +0100

    [ADD] Task 0: How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Returns the number of subscribers in a subreddit"""
+import requests
+from sys import argv
+
+
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers"""
+    base_url = "https://oauth.reddit.com/"
+    api_url = f"r/{subreddit}/about.json"
+
+    try:
+        response = requests.get(base_url + api_url)
+        response = response.json()
+        subscribers = response['data']['subscribers']
+        return subscribers
+    except Exception as E:
+        return 0
