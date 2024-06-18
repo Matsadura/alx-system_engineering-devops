e753e03fd7755f91a2262c2edd06ed2c2529a314
Matsadura <zidanditoo1@gmail.com>
2024-06-18 19:48:27 +0100

    [UPDATE] Refactor

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,10 +10,10 @@ def number_of_subscribers(subreddit):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
     }
-    try:
-        response = requests.get(base_url + api_url, headers=headers)
-        response = response.json()
-        subscribers = response['data']['subscribers']
-        return subscribers
-    except Exception as E:
+    response = requests.get(base_url + api_url, headers=headers)
+    if response.status_code != 200:
         return 0
+    response = response.json()
+    if 'dist' in response['data'].keys():
+        return 0
+    return response['data']['subscribers']
