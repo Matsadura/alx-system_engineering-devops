70ccdb2ccc1081ed0531ee40fc5b39ef342ae7a7
Matsadura <zidanditoo1@gmail.com>
2024-06-18 19:34:27 +0100

    [UPDATE] Add custom user agent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,9 +7,11 @@ def number_of_subscribers(subreddit):
     """Returns the number of subscribers"""
     base_url = "https://oauth.reddit.com/"
     api_url = f"r/{subreddit}/about.json"
-
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+    }
     try:
-        response = requests.get(base_url + api_url)
+        response = requests.get(base_url + api_url, headers=headers)
         response = response.json()
         subscribers = response['data']['subscribers']
         return subscribers
