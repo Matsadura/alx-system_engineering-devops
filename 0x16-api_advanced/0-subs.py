d9b29d551824b48c20e10658a8704111d0a811ab
Matsadura <zidanditoo1@gmail.com>
2024-06-18 19:56:48 +0100

    [UPDATE] Refactor

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
-"""Returns the number of subscribers in a subreddit"""
+
+"""
+This function is quering the Reddit API
+and returns the number of subscribers
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers"""
-    base_url = "https://api.reddit.com/"
-    api_url = f"r/{subreddit}/about"
-    headers = {
-        'User-Agent': 'Mozilla/5.0'
-    }
-    response = requests.get(base_url + api_url, headers=headers, allow_redirects=False)
+    """
+    quering the Reddit API
+    and returns the number of subscribers
+    """
+    headers = {'User-Agent': 'CustomClient/1.0'}
+    url = f"https://api.reddit.com/r/{subreddit}/about"
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         return 0
     response = response.json()
-    if 'dist' in response['data'].keys():
+    if 'data' in response:
+        return (response.get('data').get('subscribers'))
+    else:
         return 0
-    return response['data']['subscribers']
