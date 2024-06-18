ebc7d2a6811c0b5f68af868d4a36cb6e654e2e54
Matsadura <zidanditoo1@gmail.com>
2024-06-18 19:49:50 +0100

    [UPDATE] Testing

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""Returns the number of subscribers in a subreddit"""
+""" 0-subs Module
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers"""
-    base_url = "https://oauth.reddit.com/"
-    api_url = f"r/{subreddit}/about.json"
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-    }
-    response = requests.get(base_url + api_url, headers=headers)
+    """ This function return the number of subscibers of a subreddit """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    response = requests.get(url, headers={'User-Agent': 'Custom User Agent'})
+
     if response.status_code != 200:
         return 0
-    response = response.json()
-    if 'dist' in response['data'].keys():
-        return 0
-    return response['data']['subscribers']
+
+    user_info = response.json()
+
+    return user_info['data']['subscribers']
