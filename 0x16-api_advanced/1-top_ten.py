46310df69e889aa29e92c89a34fd2ca30a3a281c
Matsadura <zidanditoo1@gmail.com>
2024-06-18 20:31:37 +0100

    [UPDATE] Change 10 to 9

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,7 +6,7 @@ import requests
 def top_ten(subreddit):
     """Prints the titles of the first 10
     hot posts listed for a given subreddit"""
-    url = f"https://api.reddit.com/r/{subreddit}?sort=hot&limit=10"
+    url = f"https://api.reddit.com/r/{subreddit}?sort=hot&limit=9"
     headers = {'User-Agent': 'MyClient/1.0'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
